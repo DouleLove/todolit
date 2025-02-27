97edddcf89d41337f2150316edd3c7fdb8ca00a1
DouleLove <sashazhur108@gmail.com>
2025-02-27 23:46:39 +0300

    register urls of users application

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,9 @@
 import django.contrib
 import django.urls
 
+import users.urls
+
 urlpatterns = [
+    django.urls.path('api/user/', django.urls.include(users.urls)),
     django.urls.path('admin/', django.contrib.admin.site.urls),
 ]
