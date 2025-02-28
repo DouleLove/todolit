8b68993ebf648640262eedf3c07e27d93753f13d
DouleLove <sashazhur108@gmail.com>
2025-02-28 21:54:10 +0300

    add hash for password, add  attribute in UserAPIView because of switching to another url

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,26 @@
-import core.generics
+import django.contrib.auth.hashers
+
+import rest_framework.generics
+import rest_framework.serializers
+
 import users.models
 import users.serializers
 
 
-class UserAPIView(core.generics.CRUDAPIView,
-                  core.generics.URLKwargsMixin):
+class UserAPIView(rest_framework.generics.RetrieveUpdateDestroyAPIView,
+                  rest_framework.generics.CreateAPIView):
     serializer_class = users.serializers.UserSerializer
     queryset = users.models.User.objects.all()
-    pk_url_kwarg_name = 'uid'
+    lookup_url_kwarg = 'uid'
+
+    def perform_create(
+        self,
+        serializer: rest_framework.serializers.Serializer,
+    ) -> None:
+        # here the data validation was already done by the serializer,
+        # so the user's password is guaranteed to be valid,
+        # and we can hash and save it now
+        password_hash = django.contrib.auth.hashers.make_password(
+            self.request.data['password'],
+        )
+        serializer.save(password=password_hash)
