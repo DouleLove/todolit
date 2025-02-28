5df9941e489fd8813beb02f06da6bafcfd9daeb3
DouleLove <sashazhur108@gmail.com>
2025-02-28 21:47:08 +0300

    add endpoint for serving mediafiles

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,16 @@
+import django.conf
+import django.conf.urls.static
 import django.contrib
 import django.urls
 
 import users.urls
 
+media_urlpatterns = django.conf.urls.static.static(
+    django.conf.settings.MEDIA_URL,
+    document_root=django.conf.settings.MEDIA_ROOT,
+)
+
 urlpatterns = [
-    django.urls.path('api/user/', django.urls.include(users.urls)),
+    django.urls.path('api/users/', django.urls.include(users.urls)),
     django.urls.path('admin/', django.contrib.admin.site.urls),
-]
+] + media_urlpatterns
