e7ed0d329989ea51a544934a5aa71facb12d8c35
DouleLove <sashazhur108@gmail.com>
2025-03-24 22:08:14 +0300

    switch from default drf parser to drfseriperm BrowsableAPINoFieldsRenderer

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,3 +130,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'drfseriperm.renderers.BrowsableAPINoFieldsRenderer',
+    ],
+}
