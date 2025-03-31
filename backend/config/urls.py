ec3fdd0a76ff0ef73e9bfd617205b9966e686fcc
DouleLove <sashazhur108@gmail.com>
2025-03-31 19:36:48 +0300

    declare router in config.urls instead of making router per application

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,23 @@
 import django.conf
 import django.conf.urls.static
-import django.contrib
 import django.urls
 
-import users.urls
+import rest_framework.routers
+
+import tasks.views
+import users.views
+
+router = rest_framework.routers.DefaultRouter()
+router.register(
+    prefix='users',
+    viewset=users.views.UserViewSet,
+    basename='users',
+)
+router.register(
+    prefix='users/(?P<uid>[0-9]+)/tasks',
+    viewset=tasks.views.TaskViewSet,
+    basename='tasks',
+)
 
 media_urlpatterns = django.conf.urls.static.static(
     django.conf.settings.MEDIA_URL,
@@ -11,6 +25,5 @@ media_urlpatterns = django.conf.urls.static.static(
 )
 
 urlpatterns = [
-    django.urls.path('api/users/', django.urls.include(users.urls)),
-    django.urls.path('admin/', django.contrib.admin.site.urls),
+    django.urls.path('api/', django.urls.include(router.urls)),
 ] + media_urlpatterns
